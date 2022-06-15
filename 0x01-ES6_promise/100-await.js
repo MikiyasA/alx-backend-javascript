52cd77883736999eda65df79fd4506687c40946e
Mikiyas Adere <mikiyasad86@gmail.com>
2022-06-15 23:36:09 +0000

    To fix format error

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -1,14 +1,14 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default async function asyncUploadUser() {
-    try {
-	const result = await Promise.all([uploadPhoto(), createUser()])
-	      .then((data) => data);
-	return {
-	    photo: result[0],
-	    user: result[1],
-	};
-    } catch (e) {
-	return { photo: null, user: null };
-    }
+  try {
+    const result = await Promise.all([uploadPhoto(), createUser()])
+      .then((data) => data);
+    return {
+      photo: result[0],
+      user: result[1],
+    };
+  } catch (e) {
+    return { photo: null, user: null };
+  }
 }
